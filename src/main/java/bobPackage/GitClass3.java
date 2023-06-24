1d2c330734effab8ea9fee3530d4b5fb4d43bf82
jale <atesjale2015@gmail.com>
2023-06-23 19:42:43 -0500

    secondBranch

--- src/main/java/bobPackage/GitClass3.java
+++ src/main/java/bobPackage/GitClass3.java
@@ -3,6 +3,7 @@ package bobPackage;
 public class GitClass3 {
     public static void main(String[] args) {
         System.out.println("this is new Branch git");
+        // This is change from newBranch to secondBranch
     }
 
 }
