f394a9261f3102f368dfca3510443d90d80ef989
jale <atesjale2015@gmail.com>
2023-06-23 19:57:17 -0500

    this is three

--- src/main/java/bobPackage/GitClass3.java
+++ src/main/java/bobPackage/GitClass3.java
@@ -4,6 +4,7 @@ public class GitClass3 {
     public static void main(String[] args) {
         System.out.println("this is new Branch git");
         // This is change from newBranch to secondBranch
+        System.out.println("This is third bRanch ");
     }
 
 }
