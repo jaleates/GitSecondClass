0cb98b56f98059ec18bfeb63b19972750ac88d09
jale <atesjale2015@gmail.com>
2023-09-25 12:26:58 -0500

    new file

--- src/main/java/bobPackage/GitClass3.java
+++ src/main/java/bobPackage/GitClass3.java
@@ -4,7 +4,10 @@ public class GitClass3 {
     public static void main(String[] args) {
         System.out.println("this is new Branch git");
         // This is change from newBranch to secondBranch
+
         System.out.println("This is third bRanch ");
+
+        // this change from master after merging remote branches
     }
 
 }
